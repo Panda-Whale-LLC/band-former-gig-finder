ff068b5a183e8d2639b89ebe9e5dd1fc93ceb4f7
StephCummins <steph.a.cummins@gmail.com>
2023-11-08 21:55:38 -0800

    Updated server supertest.js

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -1,19 +1,23 @@
-const jwt = require('node-jsonwebtoken')
+const jwt = require('node-jsonwebtoken');
 const { User } = require('../models/bandFormerModels.js');
 
 const secret = `${process.env.JWT_SECRET}`;
 const authController = {};
 
 authController.createAuthJWT = (req, res, next) => {
-  if(res.locals.userId) {
-    try{
-      const token = jwt.sign({
-        id: res.locals.userId, 
-      }, secret, { expiresIn: '1h'});
+  if (res.locals.userId) {
+    try {
+      const token = jwt.sign(
+        {
+          id: res.locals.userId,
+        },
+        secret,
+        { expiresIn: '1h' }
+      );
       console.log('into token!');
       res.locals.token = token;
       return next();
-    }catch(err) {
+    } catch (err) {
       return next({
         log: `cookieController.startSessionCookie: An error occured > ${err}`,
         status: 500,
@@ -27,13 +31,12 @@ authController.createAuthJWT = (req, res, next) => {
       message: { err: 'An error occurred' },
     });
   }
-  
 };
 
 authController.verifyJWT = async (req, res, next) => {
   const token = req.get('authorization');
   if (token) {
-    try{
+    try {
       const decoded = jwt.verify(token, secret);
       const result = await User.findById(decoded.id);
       if (result !== null) {
@@ -41,7 +44,7 @@ authController.verifyJWT = async (req, res, next) => {
         res.locals.loggedIn = true;
         res.locals.userId = decoded.id;
         res.locals.body = req.body;
-        console.log('body set', res.locals.body)
+        console.log('body set', res.locals.body);
         return next();
       } else {
         //handle this redirect on the client side
@@ -49,14 +52,16 @@ authController.verifyJWT = async (req, res, next) => {
         res.locals.loggedIn = false;
         res.locals.userId = decoded.id;
         res.locals.body = req.body;
-        console.log('body set', res.locals.body)
-        return next(next({
-          log: `authController.verifyJWT: JWT is invalid, user needs to login again.`,
-          code: 400,
-          message: {err: 'New Login required.'},
-        }));
+        console.log('body set', res.locals.body);
+        return next(
+          next({
+            log: `authController.verifyJWT: JWT is invalid, user needs to login again.`,
+            code: 400,
+            message: { err: 'New Login required.' },
+          })
+        );
       }
-    } catch(err) {
+    } catch (err) {
       return next({
         log: `authController.verifyJWT: An error occured > ${err}`,
         status: 500,
@@ -64,13 +69,12 @@ authController.verifyJWT = async (req, res, next) => {
       });
     }
   }
-}
-
+};
 
 authController.verifyJWTBody = async (req, res, next) => {
   const { token } = req.body;
   if (token) {
-    try{
+    try {
       const decoded = jwt.verify(token, secret);
       const result = await User.findById(decoded.id);
       if (result !== null) {
@@ -85,18 +89,18 @@ authController.verifyJWTBody = async (req, res, next) => {
         res.locals.loggedIn = false;
         res.locals.userId = decoded.id;
         res.locals.body = req.body;
-        console.log('body set', res.locals.body)
+        console.log('body set', res.locals.body);
         return next({
           log: `authController.verifyJWT: JWT is invalid, user needs to login again.`,
           code: 400,
-          message: {err: 'New Login required.'},
+          message: { err: 'New Login required.' },
         });
       }
-    } catch(err) {
+    } catch (err) {
       res.locals.loggedIn = false;
       return next();
     }
   }
-}
+};
 
 module.exports = authController;
--- src/server/testing/supertest.js
+++ src/server/testing/supertest.js
@@ -1,49 +1,44 @@
 const request = require('supertest');
 const server = require('../server.js');
 
-const fs = require('fs');
-const path = require('path');
-const { Aggregate } = require('mongoose');
-const testDB = path.resolve(__dirname, './testUsersDB.json');
-
-describe('usersRouter tests', () => {
-
+// To test, run the following in the console:
+// jest --testMatch '**/supertest.js' --detectOpenHandles
+describe('server router tests', () => {
+  // Kill the server after all tests run
   afterAll(() => {
     server.close();
   });
 
+  // tests for users router
   describe('/users/', () => {
-
     describe('POST', () => {
-      it('responds with 200 status when a new users is created and response body includes a token', () => {
+      it('responds with 200 status when a new users is created, the response body is an Object, and it includes user and token properties', () => {
         const newUser = {
           name: 'Pikachu',
-          userName: 'UserTest12',
+          // usernames are unique! Update each test (there's probably a better way to do this)
+          userName: 'ElectroMan30',
           password: 'Charizard_is_my_homie',
         };
 
         return request(server)
-          .post('/users')
+          .post('/users/')
           .send(newUser)
           .expect('Content-Type', /application\/json/)
           .expect(200)
           .then((response) => {
-            console.log(response.body);
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('user');
             expect(response.body).toHaveProperty('token');
           })
-          .catch ((error) => {
-            //console.log(error);
+          .catch(() => {
             if (AggregateError) {
               console.log('AggregateError at /users/ post request');
             }
           });
       });
-    });
 
-    describe('POST', () => {
-      it ('responds with an error if a user is missing a name, userName or password', () => {
+      it('responds with an error if a user is missing a name, userName or password', () => {
         const newUser = {};
-
         return request(server)
           .post('/users')
           .send(newUser)
@@ -52,8 +47,7 @@ describe('usersRouter tests', () => {
           .then((response) => {
             expect(response.body).toHaveProperty('err');
           })
-          .catch ((error) => {
-            console.log(error);
+          .catch(() => {
             if (AggregateError) {
               console.log('AggregateError at /users/ post request');
             }
@@ -61,41 +55,151 @@ describe('usersRouter tests', () => {
       });
     });
 
+    describe('DELETE', () => {
+      it('responds with 200 status when a user is successfully deleted, and the user added should be the same one that is deleted', async () => {
+        // create a new user object
+        const newUser = {
+          name: 'PandaWhale',
+          userName: 'BearFish111',
+          password: 'pAnDaWhAlE4LiFe',
+        };
+
+        // create a new user
+        const response = await request(server).post('/users/').send(newUser);
+
+        // test that deleting the new user works
+        return request(server)
+          .delete(`/users/${response.body.user._id}`)
+          .set('authorization', response.body.token)
+          .expect(200)
+          .then((res) => {
+            expect(res.body).toBeInstanceOf(Object);
+            expect(res.body.name).toEqual(newUser.name);
+            expect(res.body.userName).toEqual(newUser.userName);
+            expect(res.body.user_id).toEqual(newUser.user_id);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    });
+  });
+
+  // tests for posts router
+  describe('/posts/', () => {
+    describe('GET', () => {
+      it('responds with 200 status when a new post, the response body is an array, and it includes _id, title, description, and user_id properties', () => {
+        return request(server)
+          .get('/posts/')
+          .expect('Content-Type', /application\/json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toBeInstanceOf(Array);
+            response.body.forEach((nestedObj) => {
+              expect(nestedObj).toHaveProperty('_id');
+              expect(nestedObj).toHaveProperty('title');
+              expect(nestedObj).toHaveProperty('description');
+              expect(nestedObj).toHaveProperty('user_id');
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+            if (AggregateError) {
+              console.log('AggregateError at /posts/ get request');
+            }
+          });
+      });
+    });
+
+    describe('POST', () => {
+      it('responds with 200 status when a new post is created, the response body is an Object, and it includes title, description, user_id, and _id properties', () => {
+        const newPost = {
+          title: 'TEST EVENT AGAIN!!!',
+          dates: '2025-10-19T23:15:30.000Z',
+          description: 'THIS IS ANOTHER TEST',
+          user_id: '654adb488b2d7c398c8f8b83',
+        };
+
+        return request(server)
+          .post('/posts/')
+          .send(newPost)
+          .expect('Content-Type', /application\/json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('title');
+            expect(response.body).toHaveProperty('description');
+            expect(response.body).toHaveProperty('dates');
+            expect(response.body).toHaveProperty('user_id');
+            expect(response.body).toHaveProperty('_id');
+          })
+          .catch((error) => {
+            console.log(error);
+            if (AggregateError) {
+              console.log('AggregateError at /posts/ post request');
+            }
+          });
+      });
+    });
+
+    describe('PATCH', () => {
+      it('responds with 200 status when a new post is created and response body includes a title property', () => {
+        const id = '654c155990bd588942cb80fd';
+        const updatedPost = {
+          title: 'Updated Post',
+          dates: '2025-11-19T23:15:30.000Z',
+          description: 'Yay updates!',
+        };
+        return request(server)
+          .patch(`/posts/${id}`)
+          .send(updatedPost)
+          .expect('Content-Type', /application\/json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('_id');
+            expect(response.body).toHaveProperty('title');
+            expect(response.body).toHaveProperty('description');
+            expect(response.body).toHaveProperty('user_id');
+            expect(response.body.title).toEqual(updatedPost.title);
+          })
+          .catch((error) => {
+            console.log(error);
+            if (AggregateError) {
+              console.log('AggregateError at /posts/ get request');
+            }
+          });
+      });
+    });
+
+    describe('DELETE', () => {
+      it('responds with 200 status when a post is successfully deleted, and the post added should be the same one that is deleted', async () => {
+        // create a new post object
+        const newPost = {
+          title: 'AWESOME SHOW',
+          dates: '2023-12-19T23:15:30.000Z',
+          description: 'BEST SHOW EVER',
+          user_id: '654adb488b2d7c398c8f8b83',
+        };
+
+        // post the new post
+        const response = await request(server).post('/posts/').send(newPost);
+
+        // test that deleting the new post works
+        return request(server)
+          .delete(`/posts/${response.body._id}`)
+          .expect(200)
+          .then((res) => {
+            expect(response.body).toBeInstanceOf(Object);
+            expect(res.body.title).toEqual(newPost.title);
+            expect(res.body.dates).toEqual(newPost.dates);
+            expect(res.body.description).toEqual(newPost.description);
+            expect(res.body.user_id).toEqual(newPost.user_id);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    });
   });
 });
-
-
-
-
-
-// const request = require('supertest');
-// const server = 'http://localhost:3000';
-
-// describe('usersRouter tests', () => {
-//   describe('/users/', () => {
-//     describe('POST', () => {
-//       it('responds with 200 status when a new user is created', () => {
-//         const newUser = {
-//           name: 'Pikachu',
-//           userName: 'UserTest2',
-//           password: 'Charizard_is_my_homie',
-//         };
-
-//         // Send a POST request to create a new user
-//         return request(server)
-//           .post('/users/')
-//           .send(newUser)
-//           .expect('Content-Type', /application\/json/)
-//           .expect(200)
-//           .then((response) => {
-//             // You can add assertions here to check the response
-//             expect(response.body).toHaveProperty('token');
-//           })
-//           .catch((error) => {
-//             // Handle the error properly
-//             console.error(error);
-//           });
-//       });
-//     });
-//   });
-// });
